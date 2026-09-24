6494f1e4607519d942cd677f3f836e41a2792c8a
Ziiziiy <azhicryvep80@gmail.com>
2026-09-24 21:17:37 +0700

    fix banget input kalender

--- lib/screens/konversi_tanggal_screen.dart
+++ lib/screens/konversi_tanggal_screen.dart
@@ -198,7 +198,7 @@ class _KonversiUmurState extends State<_KonversiUmur> {
       context: context,
       initialDate: _tanggalLahir,
       firstDate: DateTime(1500),
-      lastDate: DateTime.now(),
+      lastDate: DateTime(2500),
       builder: (context, child) {
         return Theme(
           data: Theme.of(context).copyWith(
